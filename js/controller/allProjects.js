368f07ee924c4e60f73b18873884805123d87a0f
onurv12 <m.onur_vural@hotmail.com>
2014-08-12 18:28:24 +0200

    Add open/close and delete buttons to open/close and delete projects to dashboard and all projects view, add those methods to corresponding controllers and projects factory

--- js/controller/allProjects.js
+++ js/controller/allProjects.js
@@ -1,11 +1,27 @@
-app.controller("allProjectsController", function ($scope, $rootScope, projectsFactory) {
+app.controller("allProjectsController", function ($scope, $rootScope, projectsFactory, userFactory) {
 	$rootScope.updateHeader();
+	$scope.moderatorLoggedIn = false;
+	userFactory.update(function(data) {
+		$scope.moderatorLoggedIn = userFactory.isModerator();
+	});
 	$scope.fetchAllProjects = function() {
 		projectsFactory.getAllProjects().then(function(data) {
 				$scope.allProjects = data;
 			}, function(error) {
 				$scope.error = error;
 			});
+	};
+	$scope.closeProject = function(projectID) {
+		projectsFactory.closeProject(projectID);
+		$scope.fetchAllProjects();
+	};
+	$scope.openProject = function(projectID) {
+		projectsFactory.openProject(projectID);
+		$scope.fetchAllProjects();
+	};
+	$scope.deleteProject = function(projectID) {
+		projectsFactory.deleteProject(projectID);
+		$scope.fetchAllProjects();
 	};
 	$scope.fetchAllProjects();
 });;
